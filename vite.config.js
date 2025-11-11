37679a1e195a35e8c5484de5fa3c5230bf05716f
hernanveyret <hernanveyret@hotmail.com>
2025-11-11 11:56:30 -0300

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,48 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'prompt', // no registra automáticamente el SW
+      injectRegister: false,  // evita que VitePWA inyecte el registro
+      workbox: undefined,     // desactiva la caché completamente
+      includeAssets: [
+        'favicon.ico',
+        'robots.txt',
+        'apple-touch-icon.png',
+        'icons/icon-192x192.png',
+        'icons/icon-512x512.png'
+      ],
+      manifest: {
+        name: 'Kiosco',
+        short_name: 'Kiosco',
+        description: 'Una app para gestion de kiosco',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#25D366',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ]
 })
